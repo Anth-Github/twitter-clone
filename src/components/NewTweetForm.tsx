3add548f98881767d8779b77ace5f9b52f69ddc1
prakash <prakash.anton31@gmail.com>
2023-05-13 13:34:48 +0530

    login profile image

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,17 +1,22 @@
+import { useSession } from "next-auth/react";
 import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
 
 export function NewTweetForm() {
+  const session = useSession();
+
+  if (session.status !== "authenticated") return;
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        {/* <ProfileImage src='#url' /> */}
+        <ProfileImage src={session.data.user.image} />
         <textarea
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="what's Happening"
         />
       </div>
 
-      <Button>Tweet</Button>
+      <Button className="self-end">Tweet</Button>
     </form>
   );
 }
